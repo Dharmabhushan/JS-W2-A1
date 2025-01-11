835b583d5b0776f2303bad55365980b6a5a2a410
Lenovo <Lenovo@OJAL>
2025-01-11 18:04:20 +0530

    updates

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,12 @@
 
 function upDate(previewPic) {
+
+    // Debugging to check if the event is triggered and print out the alt and source
+    console.log("Mouse over triggered!");
+    console.log("Alt text: " + previewPic.alt);
+    console.log("Source: " + previewPic.src);
+
+
     // 1. Change the URL for the background image of the div with id="image"
     document.getElementById('image').style.backgroundImage = 'url(' + previewPic.src + ')';
     
@@ -10,9 +17,14 @@ function upDate(previewPic) {
 
 // Function to reset the image and text in the div with id="image"
 function unDo() {
+
+     // Debugging to check if the event is triggered
+     console.log("Mouse out triggered!");
+
+
     // 1. Reset the URL for the background image of the div with id="image" to the original image
     //document.getElementById('image').style.backgroundImage = "url('')";
-    document.getElementById('image').style.backgroundImage = 'url("https://via.placeholder.com/575x650")'; // Original image
+    document.getElementById('image').style.backgroundImage = 'url(" ")'; // Original image
     
     // 2. Reset the text of the div with the id="image" back to the original text 
     document.getElementById('image').innerHTML = 'Hover over an image below to display here.'; // Original text
